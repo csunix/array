c273b267c100931ff9c8fe3b6ff9ca5b7a1f6580
Nicholas <135126451+youghage@users.noreply.github.com>
2023-05-31 13:43:18 +0100

    Update package.manage.lua

--- src/package.manage.lua
+++ src/package.manage.lua
@@ -1,1 +1,14 @@
+-- @move => parent
+-- package.manage.lua
 
+--.ignore
+
+local UtilModule = {}
+
+function UtilModule.new(utilityPackage: string)
+	if script:FindFirstChild(utilityPackage) then
+		return require(script:FindFirstChild(utilityPackage))
+	end
+end
+
+return UtilModule.new
